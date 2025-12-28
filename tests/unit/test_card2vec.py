9f0c947dfe796290937c957e999c1a79712aeacf
TristanGchlt <tristangchlt@gmail.com>
2025-12-28 05:18:02 +0100

    correction test card2vec

--- tests/unit/test_card2vec.py
+++ tests/unit/test_card2vec.py
@@ -4,7 +4,7 @@ def test_train_card2vec_dimension(card2vec_config, small_decks):
 
     model = train_card2vec(small_decks, card2vec_config)
 
-    assert model.wv.vector_size == 10
+    assert model.wv.vector_size == 8
     assert "A" in model.wv
 
 def test_extract_card_embeddings_shape(card2vec_config, small_decks):
@@ -13,4 +13,4 @@ def test_extract_card_embeddings_shape(card2vec_config, small_decks):
     vectors = extract_card_embeddings(model)
 
     assert len(vectors) == 4
-    assert all(v.shape == (10,) for v in vectors.values())
+    assert all(v.shape == (8,) for v in vectors.values())
